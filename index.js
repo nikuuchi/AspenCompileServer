b90ca98a5b25b03f3ae682e345d83e91953b33f3
uchida <uchida.atsushi.8@gmail.com>
2014-09-30 08:36:03 +0900

    hello world server

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 ///<reference path="typings/node/node.d.ts" />
-
 var http = require('http');
 var config = require('config');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end('hello\n');
 });
-
 
 console.log(config);
 console.log("server start port:" + config.server.port);
 server.listen(config.server.port, config.server.host);
-
