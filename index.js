2d192990da4163bb4856e42af5f37a1a595f1a66
uchida <uchida.atsushi.8@gmail.com>
2014-10-02 02:03:06 +0900

    work compile

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 ///<reference path="typings/node/node.d.ts" />
 var http = require('http');
+var crypto = require('crypto');
 var config = require('config');
 var exec = require('child_process').exec;
 var fs = require('fs');
 var path = require('path');
 
+function getTempFilePath(prefix) {
+    return config.out.path + prefix + crypto.randomBytes(8).readUInt32LE(0) + '.c';
+}
+
 var dispatchMap = {
     "/compile": function (req, res) {
-        var command = config.emcc.env + ' ' + config.emcc.path + ' ';
-        console.log(req.body);
-        var child = exec(config.emcc.path + ' ', function (error, stdout, stderr) {
-            var j = { error: error, message: stdout };
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(JSON.stringify(j));
-            res.end('\n');
+        var command = config.emcc.env + ' ' + config.emcc.path + ' ' + config.emcc.option + ' ';
+        var tempfile = getTempFilePath("___");
+
+        //ファイルの保存->コンパイル->(mongo)->リターン
+        fs.writeFileSync(tempfile, req.body.source);
+        var exec_command = command + ' ' + tempfile + ' -o ' + tempfile + '.js';
+        exec(exec_command, function (error, stdout, stderr) {
+            fs.readFile(tempfile + '.js', function (err, data) {
+                var j = { error: stderr, message: stdout, source: data.toString() };
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.write(JSON.stringify(j));
+                res.end('\n');
+            });
         });
     }
 };
 
 var server = http.createServer(function (req, res) {
+    console.log(req.method);
     var data = "";
     req.on('data', function (chunk) {
         data += chunk.toString();
@@ -34,7 +46,7 @@ var server = http.createServer(function (req, res) {
                 m(req, res);
             } else {
                 res.writeHead(404, { 'Content-Type': 'application/json' });
-                res.end('{}\n');
+                res.end('Not found\n');
             }
         } catch (e) {
             res.writeHead(400, { 'Content-Type': 'text/plain' });
