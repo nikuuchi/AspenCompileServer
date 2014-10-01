0d849a46af2608f067c6055f019deec88e291040
uchida <uchida.atsushi.8@gmail.com>
2014-10-01 16:08:34 +0900

    add routing

--- index.ts
+++ index.ts
@@ -2,9 +2,46 @@
 
 var http = require('http');
 var config = require('config');
+var exec = require('child_process').exec;
+var fs = require('fs');
+var path = require('path');
+
+var dispatchMap = {
+    "/compile" : function(req, res) {
+        var command = config.emcc.env + ' ' + config.emcc.path + ' ';
+        console.log(req.body);
+        var child = exec(config.emcc.path + ' ', function(error, stdout, stderr) {
+            var j = { error: error, message: stdout };
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(j));
+            res.end('\n');
+        });
+    }
+};
+
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end('hello\n');
+    var data = "";
+    req.on('data', function(chunk) {
+        data += chunk.toString();
+    });
+
+    req.on('end', function() {
+        var body = "";
+        try {
+            body = JSON.parse(data);
+            req.body = body;
+            var m = dispatchMap[req.url];
+            if(m) {
+                m(req, res);
+            } else {
+                res.writeHead(404, {'Content-Type': 'application/json'});
+                res.end('{}\n');
+            }
+        } catch(e) {
+            res.writeHead(400, {'Content-Type': 'text/plain'});
+            res.end('Bad Request\n');
+        }
+    });
 });
 
 
